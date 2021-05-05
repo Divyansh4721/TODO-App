3461d2f58cdf768d24393722ec20d00f73bd5071
Divyansh <54841396+Divyansh4721@users.noreply.github.com>
2021-05-06 02:23:16 +0530

    Delete .gitignore directory

--- .gitignore/config/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose');
-
-mongoose.connect('mongodb://localhost/TODOAppdb');
-
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error db:'));
-db.once('open', function() {
-});
